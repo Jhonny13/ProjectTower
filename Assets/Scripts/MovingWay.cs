8e55003e8dd552351eac81bcb4bb1f0999f8ce25
Zorgios <31472268+Zorgios@users.noreply.github.com>
2018-03-01 21:30:41 +0530

    1
    1

---
+++ Assets/Scripts/MovingWay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWay : MonoBehaviour {
+
+	public float Speed;
+
+	public Transform[] Waypoints; 
+
+	int curWaypoint = 0;
+
+
+	
+
+	void Update () {
+		if(curWaypoint < Waypoints.Length){
+		
+			transform.position = Vector3.MoveTowards  (transform.position, Waypoints [curWaypoint], Time.deltaTime * Speed);
+					transform.LookAt(Waypoints[curWaypoint]);
+					if(Vector3.Distance(transform.position,Waypoints[curWaypoint].position < 0.5f)){
+						
+						curWaypoint++;
+	}
+	}
+	}
+}
---
+++ Assets/Scripts/Wawe Spawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaweSpawn : MonoBehaviour {
+	
+	public int Wavesize;
+
+	public GameObject EnemyPrefab;
+
+	public float EnemyInterval;
+
+	public Transform spawnPoint;
+
+	public float startTime;
+
+	int enemyCount = 0;
+
+	public Transform[] Way;
+
+	void Start () {
+		InvokeRepeating ("SpawnEnemy",startTime,EnemyInterval);
+	}
+	
+
+	void Update () {
+		if(enemyCount == Wavesize)
+		{
+			CancelInvoke("SpawnEnemy");
+		}
+	}
+
+	void SpawnEnemy () {
+
+		GameObject enemy = GameObject.Instantiate (EnemyPrefab,spawnPoint.position,Quaternion.identity) as GameObject;
+		enemy.GetComponent<MovingWay> ().Waypoints = Way;
+	}
+
+}
+
